bcbc0f9f71bfd2093ea065ab9c46b2453da93ca5
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 10:56:31 -0300

    java one

---
+++ index.java
@@ -0,0 +1,29 @@
+//Projeto teste de conhecimento em Java!!!
+
+public class Meuscript{
+
+    {
+
+    //Declaração e inicialização de variáveis
+
+    int x = 7;
+
+    int y = 8;
+
+    int z = 3;
+
+    //Operações 
+
+    int t = x+y+z;
+    int w = x*y*z;
+    double m = x (double)y/z; //// Garante que a divisão seja feita como double para obter um resultado preciso
+
+    //Exibindo os resultados
+
+    System.out.println("O Resultado de t:",t)
+    System.out.println("O Resultado de w:",w)
+    System.out.println("O Resultado de m:",m)
+
+    }
+}
+
