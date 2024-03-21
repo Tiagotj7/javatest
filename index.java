af7174bee568866811c6ddd45ecc8ca2db2f00da
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 11:11:30 -0300

    endd

--- index.java
+++ index.java
@@ -1,30 +1,31 @@
 //Projeto teste de conhecimento em Java!!!
 
-public class Meuscript{
+public class index {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    //Declaração e inicialização de variáveis
+        // Declaração e inicialização de variáveis
 
-    int x = 7;
+        int x = 7;
 
-    int y = 8;
+        int y = 8;
 
-    int z = 3;
+        int z = 3;
 
-    //Operações 
+        // Operações
 
-    int t = x+y+z;
-    int w = x*y*z;
-    double m = x (double)y/z; //// Garante que a divisão seja feita como double para obter um resultado preciso
+        int t = x + y + z;
+        int w = x * y * z;
+        double m = x + (double) y / z; //// Garante que a divisão seja feita como double para obter um resultado
+                                       //// preciso
 
-    //Exibindo os resultados
+        // Exibindo os resultados
 
-    System.out.println("O Resultado de t:",t)
-    System.out.println("O Resultado de w:",w)
-    System.out.println("O Resultado de m:",m)
+        System.out.println("O Resultado de t:" + t);
+        System.out.println("O Resultado de w:" + w);
+        System.out.println("O Resultado de m:" + m);
 
     }
 }
 
-//The end
+// The end
