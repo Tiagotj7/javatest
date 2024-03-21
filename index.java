e1cee2a6c9c24f3e278aac2e6e9225774cac3578
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 11:13:48 -0300

    the end.

--- index.java
+++ index.java
@@ -16,8 +16,8 @@ public class index {
 
         int t = x + y + z;
         int w = x * y * z;
-        double m = x + (double) y / z; //// Garante que a divisão seja feita como double para obter um resultadoreciso
-                                      
+        double m = x + (double) y / z; //// Garante que a divisão seja feita como double para obter um resultado
+                                       //// preciso.
 
         // Exibindo os resultados
 
